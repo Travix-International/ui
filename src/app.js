f9001c187b195551066b9b99918df03b381388d1
Ricardo Machado <pt.n00b@gmail.com>
2016-11-03 17:39:06 +0100

    Changes `processors` to `const` and improves tests (#3)
    * Changes processors variable to be a const instead of let. Removed the processor variable as we don't repeat it anywhere.
    
    * Added 2 tests to improve test coverage (before it was not covering the null cases). Also updates the snapshot

--- src/app.js
+++ src/app.js
@@ -9,19 +9,15 @@ const defaultProcessors = {
 };
 
 module.exports = function app(themeYaml, format, config = {}) {
-  let processors = defaultProcessors;
-  if (config.processors) {
-    processors = Object.assign({}, config.processors, defaultProcessors);
-  }
+  const processors = Object.assign({}, config.processors || {}, defaultProcessors);
 
   if (!processors[format]) {
     throw new Error(`Missing processors for "${format}" format`);
   }
 
   try {
-    const processor = processors[format];
     const jsonTheme = yaml.safeLoad(themeYaml);
-    return processor.compile(jsonTheme, config.prefix ? config.prefix : '');
+    return processors[format].compile(jsonTheme, config.prefix ? config.prefix : '');
   } catch (error) {
     console.warn(`Failed to process theme with ${format} format. Reason:`);
     console.warn(error);
