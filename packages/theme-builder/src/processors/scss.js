f110cec932f8ff145c6b3e758f23e8b96a9ff7c8
Artem Riasnianskyi <asci@yandex.ru>
2016-11-03 13:52:43 +0100

    Moving from bitbucket to github code base with some improvements (#1)
    * tests with 100% cov, and readme file
    
    * Some fixes regard to comments from core
    
    * Updated CONTRIBUTING.md
    
    * Updated tests and added default processor
    
    * PR comment fixes

---
+++ packages/theme-builder/src/processors/scss.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ScssProcessor = {
+  compile: function recursionCompile(obj, path) {
+    const keys = Object.keys(obj);
+    let result = [];
+
+    keys.forEach((key) => {
+      if (typeof obj[key] === 'string') {
+        result.push(`$${path}-${key}: ${obj[key]};`);
+        return;
+      }
+
+      const children = recursionCompile(obj[key], path ? `${path}-${key}` : key);
+      result = result.concat(children);
+    });
+
+    return result;
+  }
+};
+
+module.exports = ScssProcessor;
