4ff14502de07796f21c89eda57f3f15a47222335
Artem Riasnianskyi <asci@yandex.ru>
2016-11-22 17:35:25 +0100

    vayama and budgetair POC for button

--- packages/themes/index.js
+++ packages/themes/index.js
@@ -1,39 +1,52 @@
 const yaml = require('yaml-ast-parser');
 const merge = require('lodash.merge');
 const fs = require('fs');
-const rootNames = ['common', 'brand', 'locale'];
+const path = require('path');
+const rootNames = ['common', 'brand', 'country'];
 
 function buildJsonTree(ast) {
-    function getValue(obj, curr) {
-      if (obj.mappings && obj.mappings.length) {
-        obj.mappings.forEach(({key, value}) => {
-          if (!value.mappings || !value.mappings.length) {
-            if (value.referencesAnchor) {
-              curr[key.value] = `*${value.referencesAnchor}`;
-              return;
-            }
-            if (value.anchorId) {
-              curr[key.value] = `&${value.anchorId} ${value.rawValue || value.value}`;
-              return;
-            }
-            curr[key.value] = value.rawValue || value.value;
-          } else {
-            curr[key.value] = getValue(value, {});
-          }
-        });
-      }
+  if (!ast) {
+    throw new Error('No AST found, please contact developer');
+  }
+  
+  if (ast.errors && ast.errors.length) {
+    ast.errors.map((err) => {
+      console.log(err.reason);
+    });
 
-      return curr;
+    throw new Error('There are some errors in AST, please contact developer');
+  }
+
+  function getValue(obj, curr) {
+    if (obj.mappings && obj.mappings.length) {
+      obj.mappings.forEach(({key, value}) => {
+        if (!value.mappings || !value.mappings.length) {
+          if (value.referencesAnchor) {
+            curr[key.value] = `*${value.referencesAnchor}`;
+            return;
+          }
+          if (value.anchorId) {
+            curr[key.value] = `&${value.anchorId} ${value.rawValue || value.value}`;
+            return;
+          }
+          curr[key.value] = value.rawValue || value.value;
+        } else {
+          curr[key.value] = getValue(value, {});
+        }
+      });
     }
 
-    return getValue(ast, {});
+    return curr;
+  }
+
+  return getValue(ast, {});
 }
 
-function getPathes(brand, lang, country) {
+function getPathes(brand, lang = '', country = '') {
   return [
-    './themes/_default.yaml',
-    `./themes/${brand}/_default.yaml`,
-    `./themes/${brand}/${lang}_${country}.yaml`
+    path.join(__dirname, '/themes/_default.yaml'),
+    path.join(__dirname, `/themes/${brand}/_default.yaml`),
+    path.join(__dirname, `/themes/${brand}/${country.toUpperCase()}.yaml`)
   ];
 }
 
