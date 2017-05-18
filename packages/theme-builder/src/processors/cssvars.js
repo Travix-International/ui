968742bc1e474c4ecfbec0825914abff488fbc00
Artem Riasnianskyi <asci@yandex.ru>
2017-05-18 08:37:22 +0200

    Add CSSVars and JSFlat processors (#43)

---
+++ packages/theme-builder/src/processors/cssvars.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function recursionCompile(obj, path) {
+  const keys = Object.keys(obj);
+  let result = [];
+
+  keys.forEach((key) => {
+    if (typeof obj[key] === 'string') {
+      result.push(`\t--${path}-${key}: ${obj[key]};`);
+      return;
+    }
+
+    const children = recursionCompile(obj[key], path ? `${path}-${key}` : key);
+    result = result.concat(children);
+  });
+
+  return result.join('\n');
+}
+
+const CssVarsProcessor = {
+  compile(obj, path) {
+    return `:root {\n${recursionCompile(obj, path)}\n}`;
+  }
+};
+
+module.exports = CssVarsProcessor;
