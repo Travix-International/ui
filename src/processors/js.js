4bb216a9ef645cdf959e63f7fccbab1777eb1899
Marcel Miranda Ackerman <m@reaktivo.com>
2017-10-13 15:06:23 +0200

    Allow accessing parent tree when referencing variables (#52)
    * Allow applying transformations on object trees
    
    * Allow accesing the parent prototype chain when referencing variables

--- src/processors/js.js
+++ src/processors/js.js
@@ -27,7 +27,7 @@ function applyTransforms(value, obj) {
  * @param {Object} the parent object
  */
 function parseExpressions(obj, proto) {
-  const parsedObj = Object.create(proto);
+  const parsedObj = Object.assign(Object.create(proto), obj);
   Object.keys(obj).forEach((key) => {
     const value = obj[key];
     const fn = isObject(value) ? parseExpressions : applyTransforms;
--- tests/specs/processors/js.spec.js
+++ tests/specs/processors/js.spec.js
@@ -3,13 +3,16 @@ const jsProcessor = require('../../../src/processors/js.js');
 describe('JS process', () => {
   it('should process object', () => {
     const doc = {
+      another: {
+        something: 'bbb'
+      },
       do: {
         some: {
           string: 'aaa',
           num: '12px',
           color: '#fff',
           // eslint-disable-next-line no-template-curly-in-string
-          template: '${color}${string}'
+          template: '${color}${some.string}${another.something}'
         }
       }
     };
