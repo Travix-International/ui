31e6315d93b48f38876b0343675849c5e647a8dd
Yuri Drabik <yurist38@mail.ru>
2018-07-17 11:24:53 +0200

    Add class name prop for main container in Badge (#31)

--- packages/travix-ui-kit/components/badge/badge.js
+++ packages/travix-ui-kit/components/badge/badge.js
@@ -15,6 +15,7 @@ const Badge = (props) => {
     border,
     children,
     className,
+    containerClassName,
     dataAttrs = {},
     mods = [],
     position,
@@ -64,7 +65,7 @@ const Badge = (props) => {
   }
 
   return (
-    <div className={getClassNamesWithMods('ui-badge', { visible }, mods)}>
+    <div className={classnames(containerClassName, getClassNamesWithMods('ui-badge', { visible }, mods))}>
       {children}
       {badge}
     </div>
@@ -93,6 +94,10 @@ Badge.propTypes = {
    * Specify a CSS class
    */
   className: PropTypes.string,
+  /**
+   * Class name for main container
+   */
+  containerClassName: PropTypes.string,
   /**
    * Data attribute. You can use it to set up any custom data-* attribute.
    */
